5d6559b40b7b63799bc14e54d904d12af6e2740c
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-19 16:59:49 +0200

    Authorization-on-users-endpoints

--- src/main/java/elisadaria/UN5W3d5praticaS3L5/controllers/UsersController.java
+++ src/main/java/elisadaria/UN5W3d5praticaS3L5/controllers/UsersController.java
@@ -8,9 +8,13 @@ import elisadaria.UN5W3d5praticaS3L5.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/users")
@@ -19,15 +23,36 @@ public class UsersController {
     @Autowired
     UsersService usersService;
     //http://localhost:3002/users
-    @GetMapping()
+    @GetMapping("")
+    @PreAuthorize("hasAuthority('EVENT_PLANNER')")
+
     public Page<User>getUsers(
-            @RequestParam(defaultValue = "0")int page,
+            @RequestParam(defaultValue = "1")int page,
             @RequestParam(defaultValue = "15")int size,
             @RequestParam(defaultValue = "name")String sort_by
 
     ){
         return this.usersService.gettingUs(page, size, sort_by);
     }
+    //http://localhost:3002/users/me
+    @GetMapping("/me")
+    public User myProfile(@AuthenticationPrincipal User current){
+        return current;
+    }
+    @DeleteMapping("/me")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMyProfile(@AuthenticationPrincipal User current){
+        usersService.delete(current.getId());
+    }
+    //http://localhost:3002/users/id
+    @DeleteMapping("/{userId}")
+    @PreAuthorize("hasAuthority('EVENT_PLANNER')")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteU (@PathVariable long userId){
+        this.usersService.delete(userId);
+    }
+
+
 
 
 }
