9c1c464a4066562416089da3fc0e721d04bb34a5
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-19 14:56:54 +0200

    security-partially-handled

---
+++ src/main/java/elisadaria/UN5W3d5praticaS3L5/security/JWT.java
@@ -0,0 +1,32 @@
+package elisadaria.UN5W3d5praticaS3L5.security;
+
+import elisadaria.UN5W3d5praticaS3L5.entities.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWT {
+    @Value("${jwt.secret}")
+    private String secret;
+    public String creatingT(User user){
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis()+1000*60*60*24*14))
+                .subject(String.valueOf(user.getId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+    public void verifyT(String token){
+        try{
+            Jwts.parser()
+                    .verifyWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                    .build().parse(token);
+        }catch (Exception ex){
+            throw new Un
+        }
+    }
+}
