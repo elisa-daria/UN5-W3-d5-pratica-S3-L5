ebd491510dcb67f8b8324b8828c52960b9190437
elisa-daria <elisa.daria.tomasi@gmail.com>
2024-04-19 10:22:01 +0200

    repository-relocation

---
+++ src/main/java/elisadaria/UN5W3d5praticaS3L5/Un5W3D5PraticaS3L5Application.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W3d5praticaS3L5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Un5W3D5PraticaS3L5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Un5W3D5PraticaS3L5Application.class, args);
+	}
+
+}
---
+++ src/test/java/elisadaria/UN5W3d5praticaS3L5/Un5W3D5PraticaS3L5ApplicationTests.java
@@ -0,0 +1,13 @@
+package elisadaria.UN5W3d5praticaS3L5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Un5W3D5PraticaS3L5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
